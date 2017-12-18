5b27e1b77d5c779ed7459a07d2f07f145337750e
Thomas Kilmer <tkilmer95@gmail.com>
2017-12-18 14:11:05 -0800

    listing webhook

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,16 @@
 class MessagesController < ApplicationController
-  
+
   def show
     @message = Messsage.find(params[:id])
     render :show
   end
+
+  def create
+    action, data = params[action], params[data]
+    text = action.to_s + " " + data.to_s
+    @message = Messsage.new(message_text: text)
+    @message.save!
+    render :show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  
+
+
   resources :answers, only: [:new, :create, :index]
   resources :questions, only: [:show]
   resources :users, only: [:new, :create, :show]
-  
-  resources :messages, only: [:show]
-  
+
+  resources :messages, only: [:show, :create]
+
   root to: redirect('/users/new')
 end
